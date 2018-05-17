095adf954dbe09d293e0ff84eb47199596e9d764
Beck <bejuli@apac.corpdir.net>
2018-05-18 08:26:56 +0900

    upload files

---
+++ srt2anki.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+
+Module = {
+    TOTAL_MEMORY: 2000000000
+}
+const fs = require('fs');
+const path = require('path');
+const parser = require('subtitles-parser');
+const search = require('recursive-search');
+const AnkiExport = require('anki-apkg-export').default;
+const program = require('commander');
+let lineNumbers = 0;
+
+program
+    .version('0.0.1')
+    .usage('[options] <path>')
+    .option('-c, --csv', 'creates csv file instead of anki deck, please use for larger files', false)
+    .option('-m, --min [min]', 'minimum length of a sub-line to be added to the deck', 4)
+    .option('-s, --simple', 'only add sub-line ', false)
+    .option('-d, --deckname [name]', 'name of the generated deck', "deck")
+    .parse(process.argv);
+
+  
+
+if (!program.args.length) {
+    program.help();
+} else {
+    const apkg = new AnkiExport(program.deckname+".apkg");
+    const wstream = fs.createWriteStream(program.deckname+".csv");
+    let directory = program.args[0];
+    // search all files
+    console.log(`Searching for .srt Files in: ${directory}`);
+    search.recursiveSearch(/.srt$/, directory, function (err, result) {
+        if (err) throw err
+        // result
+    }, function (results) {
+
+        console.log(`Found ${results.length} Files`);
+        results.forEach(function (file, i) {
+            let srt = fs.readFileSync(file, 'utf8');
+            let data = parser.fromSrt(srt);
+            let filename = path.basename(file).slice(0, -4);
+
+            console.log(`${i}/${results.length}; Lines: ${data.length} TOTAL: ${lineNumbers}; now processing ${filename}`);
+
+            data.forEach(line => {
+                if (line.text.length >= program.min) {
+                    if (program.csv) {
+                        if (program.simple) wstream.write(`${line.text.replace(/\n|\r/g, "").replace(";","")}\n`)
+                        else wstream.write(`${filename};${line.text.replace(/\n|\r/g, "").replace(";","")}\n`);
+                    } else {
+                        if (program.simple) apkg.addCard(line, "");
+                        else apkg.addCard(filename, line);
+                    }
+                }
+            });
+            lineNumbers = lineNumbers + data.length;
+        });
+
+
+        if (program.csv) {
+            console.log(`Wrote ${lineNumbers} Lines to csv`);
+            console.log("Closing files....");
+
+        } else {
+            apkg.save()
+                .then(zip => {
+                    fs.writeFileSync('./subtitles.apkg', zip, 'binary');
+                    console.log(`Package has been generated: subtitles.apkg`);
+                })
+                .catch(err => console.log(err.stack || err));
+        }
+
+
+
+
+    })
+
+}
